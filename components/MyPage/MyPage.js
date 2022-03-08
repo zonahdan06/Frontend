802a2cfb6115dc8a6a7b186d03ab9a024c0218f3
yeah1832 <aer561@naver.com>
2022-03-08 11:33:48 +0900

    Frontend v2

--- components/MyPage/MyPage.js
+++ components/MyPage/MyPage.js
@@ -1,126 +1,54 @@
 import "./MyPage.css";
 import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
 const MyPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { id } = location.state;
   console.log("state", location.state);
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then(response => {
+      console.log(response.data);
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃 하는데 실패했습니다.");
+      }
+    });
+  };
   //const navigateState = useNavigate().state;
   //const [userId, setUserId] = useState(navigateState && navigateState.id);
   return (
     <div className="Main">
       <div className="leftBook">
         <div className="header">
-          <h1>책을 골라보아요</h1>
+          <h1>내 정보</h1>
         </div>
-        <div className="bookSelect">
-          <div
-            className="book"
-            onClick={() => {
-              navigate("/Book1");
-            }}
-          >
+
+        <div>
+          <div className="User_info">
             <img
-              className="book_img"
-              alt="book"
-              src={require("../img/book1.png")}
+              className="user_img2"
+              alt="user_img"
+              src={require("../img/user.png")}
             />
-            <div className="bookTitle">
-              <h2>토끼와 거북이</h2>
-            </div>
-          </div>
-          <div className="book">
-            <img
-              className="book_img"
-              alt="book1"
-              src={require("../img/book2.png")}
-            />
-            <div className="bookTitle">
-              <h2>아기돼지 삼형제</h2>
-            </div>
-          </div>
-          <div className="book">
-            <img
-              className="book_img"
-              alt="book"
-              src={require("../img/book1.png")}
-            />
-            <div className="bookTitle">
-              <h2>토끼와 거북이</h2>
-            </div>
-          </div>
-          <div className="book">
-            <img
-              className="book_img"
-              alt="book1"
-              src={require("../img/book2.png")}
-            />
-            <div className="bookTitle">
-              <h2>아기돼지 삼형제</h2>
-            </div>
+            <p className="user_id2"> 아이디 : {id}</p>
+            <button className="edit">수정</button>
+            <p className="logout" onClick={onClickHandler}>
+              로그아웃
+            </p>
+            <p className="withdraw">회원탈퇴</p>
           </div>
         </div>
       </div>
       <div className="line"></div>
       <div className="rightBook">
         <div className="header2">
-          <div className="user">
-            <img
-              className="user_img"
-              alt="user_img"
-              src={require("../img/user.png")}
-            />
-            <p className="user_id">{id}님</p>
-          </div>
+          <button className="goMain" onClick={() => navigate(-1)}>
+            메인화면
+          </button>
         </div>
-        <div className="bookSelect">
-          <div className="book">
-            <img
-              className="book_img"
-              alt="book"
-              src={require("../img/book1.png")}
-            />
-            <div className="bookTitle">
-              <h2>토끼와 거북이</h2>
-            </div>
-          </div>
-          <div className="book">
-            <img
-              className="book_img"
-              alt="book1"
-              src={require("../img/book2.png")}
-            />
-            <div className="bookTitle">
-              <h2>아기돼지 삼형제</h2>
-            </div>
-          </div>
-          <div className="book">
-            <img
-              className="book_img"
-              alt="book"
-              src={require("../img/book1.png")}
-            />
-            <div className="bookTitle">
-              <h2>토끼와 거북이</h2>
-            </div>
-          </div>
-          <div className="book">
-            <img
-              className="book_img"
-              alt="book1"
-              src={require("../img/book2.png")}
-            />
-            <div className="bookTitle">
-              <h2>아기돼지 삼형제</h2>
-            </div>
-          </div>
-        </div>
-        <div className="next">
-          <div className="pageCount">1 / 1</div>
-          <div className="next_bt">
-            <img alt="next_bt" src={require("../img/nextButton.png")} />
-          </div>
-        </div>
+        <div className="bookSelect"></div>
       </div>
     </div>
   );
